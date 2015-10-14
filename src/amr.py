451a435ca051305517c79216d7ab9441939f4004
mb4e10 <mb4e10@soton.ac.uk>
2015-10-14 12:16:44 +0100

    Add sigma0 and alpha AMR parameters to the function.

--- src/amr.py
+++ src/amr.py
@@ -1,6 +1,6 @@
 import dolfin as df
 
-def amr(mesh, m, DirichletBoundary, g, d):
+def amr(mesh, m, DirichletBoundary, g, d, s0=1, alpha=1):
     V = df.FunctionSpace(mesh, "CG", 1)
 
     # Define boundary condition
@@ -9,9 +9,9 @@ def amr(mesh, m, DirichletBoundary, g, d):
     # Define variational problem
     u = df.Function(V)
     v = df.TestFunction(V)
-    E = df.grad(u)
+    E = -df.grad(u)
     costheta = df.dot(m, E)
-    sigma = 1/(1 + costheta**2)
+    sigma = s0/(1 + alpha*costheta**2)
     F = df.inner(sigma*df.grad(u), df.grad(v))*df.dx
 
     # Compute solution
