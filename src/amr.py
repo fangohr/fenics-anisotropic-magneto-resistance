acbb9d9b8dbc4bddf54ee264512cd5770c0dc18d
mb4e10 <mb4e10@soton.ac.uk>
2015-10-30 12:42:02 +0000

    Do the solving of nonlinear problem explicitly.

--- src/amr.py
+++ src/amr.py
@@ -1,25 +1,44 @@
 import dolfin as df
+import numpy as np
 
-def amr(mesh, m, DirichletBoundary, g, d, s0=1, alpha=1000):
+def amr(mesh, m, DirichletBoundary, g, d, s0=1, alpha=10):
     V = df.FunctionSpace(mesh, "CG", 1)
+    VV = df.VectorFunctionSpace(mesh, 'CG', 1, 3)
 
     # Define boundary condition
-    bc = df.DirichletBC(V, g, DirichletBoundary())
+    bcs = df.DirichletBC(V, g, DirichletBoundary())
 
-    # Define variational problem
-    u = df.Function(V)
+    def sigma(u):
+        E = -df.grad(u)
+        costheta = df.dot(m, E)/df.sqrt(df.dot(E, E))
+        return s0/(1 + alpha*costheta**2)
+
+    # Define variational problem for Picard iteration
+    u = df.TrialFunction(V)
     v = df.TestFunction(V)
-    E = -df.grad(u)
-    costheta = df.dot(m, E)
-    sigma = s0/(1 + alpha*costheta**2)
-    F = df.inner(sigma*df.grad(u), df.grad(v))*df.dx
+    u_k = df.interpolate(g, V)  # previous (known) u
+    a = df.inner(sigma(u_k)*df.nabla_grad(u), df.nabla_grad(v))*df.dx
+    f = df.Constant(0.0)
+    L = f*v*df.dx
 
-    # Compute solution
-    df.solve(F == 0, u, bc, solver_parameters={"newton_solver":
-                                               {"relative_tolerance": 1e-6}})
+    # Picard iterations
+    u = df.Function(V)     # new unknown function
+    eps = 1.0           # error measure ||u-u_k||
+    tol = 1.0E-5        # tolerance
+    iter = 0            # iteration counter
+    maxiter = 25        # max no of iterations allowed
+    while eps > tol and iter < maxiter:
+        iter += 1
+        df.solve(a == L, u, bcs)
+        diff = u.vector().array() - u_k.vector().array()
+        eps = np.linalg.norm(diff, ord=np.Inf)
+        print 'iter=%d: norm=%g' % (iter, eps)
+        u_k.assign(u)   # update for next iteration
+
 
     # Plot solution and solution gradient
     df.plot(u, title="Solution")
-    df.plot(-sigma*df.grad(u), title="current density")
-    df.plot(E/df.dot(E, E))
+    df.plot(-sigma(u)*df.grad(u), title="current density")
     df.interactive()
+
+    return df.project(-sigma(u)*df.grad(u), VV)
